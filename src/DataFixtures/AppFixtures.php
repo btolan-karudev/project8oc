40d307d2ca66600578092258e2740ef711c56dd0
Beniamin Tolan thinkCenter <beniamin.tolan@outlook.com>
2020-10-15 12:02:46 +0200

    users added to fixture

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Task;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -10,11 +11,28 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        for ($i=1; $i <=100; $i++) {
+        //User management
+        $users = [];
+        for ($i = 1; $i <= 10; $i++) {
+            $user = new User();
+            $user->setUsername('anonyme');
+            $user->setEmail("anonyme$i@gmail.com");
+            $user->setPassword(null);
+
+            $manager->persist($user);
+            $users[] = $user;
+        }
+
+        // Task management
+        for ($i = 1; $i <= 100; $i++) {
             $task = new Task();
+
+            $user = $users[mt_rand(0, count($users) - 1)];
+
             $task->setTitle("Titre n°$i");
             $task->setCreatedAt(new \DateTime());
             $task->setContent("<p>Je suis le contendu de cette action</p>");
+            $task->setUser($user);
 
             $manager->persist($task);
         }
