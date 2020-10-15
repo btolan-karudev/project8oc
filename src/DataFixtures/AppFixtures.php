cbf29e7ef50b042f644f12d9e39e475e976f1d82
Beniamin Tolan thinkCenter <beniamin.tolan@outlook.com>
2020-10-15 12:30:37 +0200

    fixtures updat

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -6,18 +6,28 @@ use App\Entity\Task;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    private $encodeur;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encodeur = $encoder;
+    }
+
     public function load(ObjectManager $manager)
     {
         //User management
         $users = [];
         for ($i = 1; $i <= 10; $i++) {
             $user = new User();
-            $user->setUsername('anonyme');
+
+            $password = $this->encodeur->encodePassword($user, "123456");
+            $user->setUsername("anonyme$i");
             $user->setEmail("anonyme$i@gmail.com");
-            $user->setPassword(null);
+            $user->setPassword($password);
 
             $manager->persist($user);
             $users[] = $user;
@@ -32,7 +42,7 @@ class AppFixtures extends Fixture
             $task->setTitle("Titre n°$i");
             $task->setCreatedAt(new \DateTime());
             $task->setContent("<p>Je suis le contendu de cette action</p>");
-            $task->setUser($user);
+            $task->setAuthor($user);
 
             $manager->persist($task);
         }
