9f8f1447e9f3a976a32f74e2a2b4afc7dfc47cd6
Beniamin Tolan thinkCenter <beniamin.tolan@outlook.com>
2020-10-15 13:52:00 +0200

    #13:authentification ok

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -4,7 +4,7 @@ namespace App\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
@@ -12,8 +12,10 @@ class SecurityController extends AbstractController
 {
     /**
      * @Route("/login", name="login")
+     *
+     * @return Response
      */
-    public function loginAction(Request $request, AuthenticationUtils $authenticationUtils)
+    public function loginAction(AuthenticationUtils $authenticationUtils)
     {
 
         $error = $authenticationUtils->getLastAuthenticationError();
