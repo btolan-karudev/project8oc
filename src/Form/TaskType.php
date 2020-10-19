3c8d1740b2daed4f484d298d59f87ed4af3c430f
Beniamin Tolan thinkCenter <beniamin.tolan@outlook.com>
2020-10-19 20:07:53 +0200

    refactoring form for task

--- src/Form/TaskType.php
+++ src/Form/TaskType.php
@@ -3,6 +3,8 @@
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
@@ -11,9 +13,14 @@ class TaskType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('content', TextareaType::class)
-            //->add('author') ===> must be the user authenticated
+            ->add('title', TextType::class, [
+                'attr' => ['class' => 'form-control'],
+                'label' => 'Votre titre'
+            ])
+            ->add('content', TextareaType::class, [
+                'attr' => ['class' => 'form-control'],
+                'label' => 'Votre contenu'
+            ])
         ;
     }
 }
